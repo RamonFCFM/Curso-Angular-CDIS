c0511c20d03eb06c6a905c99ffad3e6cdfc6c734
RamonFCFM <60339237+RamonFCFM@users.noreply.github.com>
2020-05-22 09:24:28 -0500

    E1#2

--- mi-catalogo/src/app/app.module.ts
+++ mi-catalogo/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { VistasComponent } from './vistas/vistas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    VistasComponent
   ],
   imports: [
     BrowserModule
